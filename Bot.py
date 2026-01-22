d7acfb7758041f5044ac3c2f4cc88bb591e7107d
KirillVarg501 <kbutorin700@gmail.com>
2026-01-22 18:18:49 +0700

    Bot.py

--- Bot.py
+++ Bot.py
@@ -1,17 +1,15 @@
+import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
-import os
 
-TOKEN = os.getenv("BOT_TOKEN")  # токен из Render
+TOKEN = os.getenv("BOT_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Бот запущен и работает 🚀")
+    await update.message.reply_text("Бот работает ✅")
 
 def main():
     app = ApplicationBuilder().token(TOKEN).build()
-
     app.add_handler(CommandHandler("start", start))
-
     app.run_polling()
 
 if __name__ == "__main__":
